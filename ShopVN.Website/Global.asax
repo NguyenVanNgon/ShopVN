20021fa7eff2082b03e931be2a0aa62280248226
Admin\Admin <khatvongtuoitrendt@gmail.com>
2016-09-26 09:32:44 +0300

    this first commit

---
+++ ShopVN.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopVN.Website.MvcApplication" Language="C#" %>
---
+++ ShopVN.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopVN.Website.Startup))]
+namespace ShopVN.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
